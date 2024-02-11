43ded852d3b9b0faf557e56b4788e20ba7aa3563
futfetish <gr1zzly.medvegonok@gmail.com>
2024-02-11 18:33:28 +0300

    axios upd

--- src/axios/server/authServer.ts
+++ src/axios/server/authServer.ts
@@ -3,20 +3,31 @@ import { AxiosResponse } from "axios";
 import { $api } from "../api";
 import { authResponseI } from "@/service/authService";
 
-
-
 interface authServerI {
   login: (
     name: string,
     password: string
-  ) => Promise<
-    AxiosResponse<authResponseI>
-  >;
+  ) => Promise<AxiosResponse<authResponseI>>;
+
+  logout: () => void;
+
+  register: (
+    name: string,
+    password: string
+  ) => Promise<AxiosResponse<authResponseI>>;
 }
 
 class AuthServer implements authServerI {
   async login(name: string, password: string) {
     return $api.post<authResponseI>("/login", { name, password });
   }
+
+  async logout() {
+    return $api.get<void>("/logout");
+  }
+
+  async register(name : string , password : string){
+    return $api.post<authResponseI>("/register", { name, password });
+  }
 }
-export const AuthServerClient = new AuthServer()
+export const AuthServerClient = new AuthServer();
