de401a36ad046682f954b8d831faf802f6abdb15
futfetish <gr1zzly.medvegonok@gmail.com>
2024-02-12 10:19:57 +0300

    res interceptor

--- src/axios/api.ts
+++ src/axios/api.ts
@@ -1,14 +1,44 @@
 import axios from "axios";
+import { config } from "process";
 
-export const API_URL = 'http://localhost:3000/api'
+export const API_URL = "http://localhost:3000/api";
 
 export const $api = axios.create({
-    withCredentials : true,
-    baseURL : API_URL
-})
+  withCredentials: true,
+  baseURL: API_URL,
+});
 
 $api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
-    return config
-})
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    "accessToken"
+  )}`;
+  return config;
+});
 
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    console.log(1)
+    const req = error.config;
+    if (error.response) {
+      if (
+        error.response.status == 401 &&
+        error.config &&
+        !error.config._isRetry
+      ) {
+        req._isRetry = true;
+        try {
+          const res = await axios.get(`${API_URL}/refresh`, {
+            withCredentials: true,
+          });
+          localStorage.setItem("accessToken", res.data.accessToken);
+          return $api.request(req);
+        } catch  {
+            //пользователь не авторизован
+        }
+      }
+    }
+  }
+);
