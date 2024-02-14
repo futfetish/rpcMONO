510201eb14cf33a9f3e2da7c962564e3b6d072f1
futfetish <gr1zzly.medvegonok@gmail.com>
2024-02-14 11:12:50 +0300

    store : error

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,47 +1,60 @@
 import { $api, API_URL } from "@/axios/api";
 import { AuthServerClient } from "@/axios/server/authServer";
 import { UserDtoI } from "@/dtos/userDto";
+import { Err } from "@/entites/error/error";
 import { userI } from "@/entites/user/userController";
 import { authResponseI } from "@/service/authService";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { create } from "zustand";
 
 interface AuthStoreI {
   user: UserDtoI | null;
-  isLoading : boolean;
-  login: (name: string, password: string) => void;
-  register: (name: string, password: string) => void;
+  isLoading: boolean;
+  error: null | Err;
+  login: (name: string, password: string) => Promise<void | Err>;
+  register: (name: string, password: string) => Promise<void | Err>;
   logout: () => void;
-  chechAuth: () => void
+  chechAuth: () => void;
 }
 
 export const AuthStore = create<AuthStoreI>((set, get) => ({
   user: null,
-  isLoading : false,
+  isLoading: false,
+  error: null,
   login: async (name, password) => {
     const response = await AuthServerClient.login(name, password);
-    localStorage.setItem("accessToken", response.data.accessToken);
-    set({ user: response.data.user });
+    if (response instanceof AxiosError) {
+      set({error : response.response?.data})
+    } else {
+      localStorage.setItem("accessToken", response.data.accessToken);
+      set({ user: response.data.user , error : null });
+    }
   },
   register: async (name, password) => {
     const response = await AuthServerClient.register(name, password);
-    localStorage.setItem("accessToken", response.data.accessToken);
-    set({ user: response.data.user });
+    if (response instanceof AxiosError) {
+      set({error : response.response?.data})
+    } else {
+      localStorage.setItem("accessToken", response.data.accessToken);
+      set({ user: response.data.user , error : null });
+    }
   },
   logout: async () => {
     await AuthServerClient.logout();
     set({ user: null });
   },
-  chechAuth : async () => {
-    set({isLoading : true})
+  chechAuth: async () => {
+    set({ isLoading: true });
     try {
-      const userData = await axios.get<authResponseI>(`${API_URL}/refreshToken` , {withCredentials : true}) 
-      set({user : userData.data.user})
+      const userData = await axios.get<authResponseI>(
+        `${API_URL}/refreshToken`,
+        { withCredentials: true }
+      );
+      set({ user: userData.data.user });
     } catch (e) {
       //пользователь не авторизован
     } finally {
-      set({isLoading : false})
+      set({ isLoading: false });
     }
-   
-  }
+  },
 }));
