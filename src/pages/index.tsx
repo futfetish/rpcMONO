0c16ed36542a8d214434c7f780d7c782e250376e
futfetish <gr1zzly.medvegonok@gmail.com>
2024-02-25 14:02:56 +0300

    main page clean

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,22 +8,9 @@ import { MyA } from "@/UI/myA/myA";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const user = AuthStore((state) => state.user);
-  const isLoading = AuthStore((state) => state.isLoading)
-  const logout = AuthStore((state) => state.logout)
-  if (isLoading){
-    return <div>loading...</div>
-  }
   return (
     <div>
-      {user ? (
-        <div>
-          <p> вы авторизованы : {user.name} </p>
-          <MyA  onClick={() => logout()}>logout</MyA>
-        </div>
-      ) : (
-        <div>вы не авторизованы</div>
-      )}
+
     </div>
   );
 }
